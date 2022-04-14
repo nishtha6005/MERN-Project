fcc3f19e89ee6ab7ff96033ba101195e5b4c1921
nishtha6005 <nishthamaheshwari09@gmail.com>
2022-04-14 12:47:35 +0530

    Background Image on Home Page

--- Cafe App/client/src/components/home/Home.js
+++ Cafe App/client/src/components/home/Home.js
@@ -1,4 +1,5 @@
 import React,{useEffect} from "react";
+import '../../images/bg.jpg'
 
 function Home(){
 
@@ -6,9 +7,22 @@ function Home(){
         document.title='Home'
     },[])
 
+    const myStyle={
+        backgroundImage:"url('https://doitd.com/wp-content/uploads/2020/06/doitd-Restaurant-table-booking-system.jpeg')",
+        height:'100vh',
+        marginTop:'-70px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+    }
+
     return(
         <>
-            <h1 className="text-center mt-5 ">Welcome to CafeApp !!</h1>
+        <div style={myStyle}>
+          <br/><br/>
+            <h1 className="text-center text-white mt-5 ">Welcome to CafeApp!!!</h1>
+        </div>
+            
+            
         </>
     )
 }
